6e8b1e42c23e23b4491cf41ae782699f1bfbcd01
AbdullahRizwan <54895336+AbdullahRizwan@users.noreply.github.com>
2022-10-13 01:44:21 +0500

    Added model and router (#1)
    * Added model and router
    
    * Fixed review comments

---
+++ server/routes/api_routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { Verification } from "../models/verification.js";
+
+export const router = Router();
+
+router.get("/", (req, res) => {
+  const verification = new Verification({
+    email: req.body.email,
+    is_valid: false,
+    disposable_status: false,
+    verified_on: new Date(),
+  });
+  res.send(verification);
+});
