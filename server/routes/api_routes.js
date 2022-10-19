5e0af006d2ba77ddac770a1e4171fb03f359414d
AbdullahRizwan <arshad.qureshi.nbp0@gmail.com>
2022-10-19 16:07:23 +0500

    Momin Code

--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -1,11 +1,51 @@
 import { Router } from "express";
-import { Verification_Controller } from "../controllers/verification_contoller.js";
+import { VerificationController, verify_email_in_db } from "../controllers/verification_contoller.js";
 
 export const router = Router();
-const controller = new Verification_Controller();
+const controller = new VerificationController();
 
-router.get("/", (req, res) => {
+var request_counts = {
+  klean_api: 0,
+  clearout_api: 0,
+};
 
-    controller.klean_api_request(req.body.email);
-    res.send(req.body.email);
+var api_limits = {
+  klean_api: 2,
+  clearout_api: 2,
+};
+
+setInterval(() => {
+  console.log(request_counts);
+  request_counts = {
+    klean_api: 0,
+    clearout_api: 0,
+  };
+}, 20000);
+
+router.get("/", async (req, res) => {
+  let email = req.query.email;
+
+  const cached_queries = await verify_email_in_db(email);
+  console.log(cached_queries);
+  if (cached_queries.length > 0) {
+    console.log("Found");
+    res.send(cached_queries[0]);
+  }
+
+  while (
+    request_counts["klean_api"] >= api_limits["klean_api"] &&
+    request_counts["clearout_api"] >= api_limits["clearout_api"]
+  ) {
+    await new Promise((r) => setTimeout(r, 10000));
+  }
+
+  if (request_counts["klean_api"] < api_limits["klean_api"]) {
+    request_counts["klean_api"]++;
+    console.log("KLEAN", request_counts);
+    controller.klean_api_request(res, email);
+  } else {
+    request_counts["clearout_api"]++;
+    console.log("CLEAR", request_counts);
+    controller.clearout_email_verification(res, email);
+  }
 });
