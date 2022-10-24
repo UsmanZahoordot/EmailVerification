c9ddf6e4912528deb022c830c8e67c4b8597d41f
AbdullahRizwan <54895336+AbdullahRizwan@users.noreply.github.com>
2022-10-24 22:26:55 +0500

    refactored to promise based (#7)
    * refactored to promise based
    
    * refactored to promise based
    
    * refactored to promise based
    
    * Bulk emails
    
    * Bulk emails
    
    * Bulk emails

--- server/controllers/verification_contoller.js
+++ server/controllers/verification_contoller.js
@@ -1,5 +1,6 @@
 import requestModule from "request";
 import { Verification } from "../models/verification.js";
+import axios from "axios";
 
 export const verify_email_in_db = async (email) => {
   const all = await Verification.find({ email: email });
@@ -7,84 +8,65 @@ export const verify_email_in_db = async (email) => {
 };
 
 export class VerificationController {
-  klean_api_request = async (response, email) => {
+  klean_api_request = async (email) => {
     var reqBody = {
       record: email,
     };
     reqBody = JSON.stringify(reqBody);
-    requestModule.post(
-      {
-        url: "https://api.kleanmail.com/record_verification/api_record",
-        body: reqBody,
-        headers: {  
-          api_key: process.env.KLEAN_API_KEY,
-          "Content-Type": "application/json",
-        },
-      },
-      "post.bin",
-      function (err, res) {
-        if (err) {
-          response.send("Error!");
-          return;
-        }
-
-        console.log(res.body);
-        const raw_data = JSON.parse(res.body);
-        const verification = new Verification({
-          email: email,
-          is_valid: raw_data["is_exist"],
-          is_disposable: raw_data["is_disposable"],
-          verified_on: new Date(),
-        });
-        verification.save((err, doc) => {
-          if (!err) console.log("success", "User added successfully!");
-          else console.log("Error during record insertion : " + err);
-        });
-        response.send(verification);
-        return;
+    const options = {
+      headers: {  
+        api_key: process.env.KLEAN_API_KEY,
+        "Content-Type": "application/json",
       }
-    );
+    };
+
+    return axios.post("https://api.kleanmail.com/record_verification/api_record", reqBody, options).then(response => {
+      console.log(response.data);
+      const raw_data = response.data;
+      const verification = new Verification({
+        email: raw_data['record'],
+        is_valid: raw_data["is_exist"],
+        is_disposable: raw_data["is_disposable"],
+        verified_on: new Date(),
+      });
+      
+      verification.save((err, doc) => {
+        if (!err) console.log("success", "User added successfully!");
+        else console.log("Error during record insertion : " + err);
+      });
+
+      return(verification);
+    });
   };
 
-  clearout_email_verification = async (response, email) => {
+  clearout_email_verification = async (email) => {
     var reqBody = {
       email: email,
     };
     reqBody = JSON.stringify(reqBody);
-    requestModule.post(
-      {
-        url: "https://api.clearout.io/v2/email_verify/instant",
-        body: reqBody,
-        headers: {
-          Authorization: process.env.CLEAROUT_API_KEY, // Your API KEY
-          "Content-Type": "application/json",
-        },
-      },
-      "post.bin",
-      function (err, res) {
-        if (err) {
-          console.error(err);
-          response.send("Error!");
-          return;
-        }
-
-        const raw_data = JSON.parse(res.body);
-        if (raw_data["status"] == "success") {
-          const verification = new Verification({
-            email: email,
-            is_valid: raw_data["data"]["status"] == "valid" || false,
-            is_disposable: raw_data["data"]["desposible"] != "no",
-            verified_on: new Date(),
-          });
-          verification.save((err, doc) => {
-            if (!err) console.log("success", "User added successfully!");
-            else console.log("Error during record insertion : " + err);
-          });
-          response.send(verification);
-          return;
-        }
+    const options = {
+      headers: {
+        Authorization: process.env.CLEAROUT_API_KEY, // Your API KEY
+        "Content-Type": "application/json",
       }
-    );
+    }
+    return axios.post("https://api.clearout.io/v2/email_verify/instant", reqBody, options).then(response => {
+      console.log(response.data);
+      const raw_data = response.data;
+      const verification = new Verification({
+        email: email,
+        is_valid: raw_data["data"]["status"] == "valid" || false,
+        is_disposable: raw_data["data"]["desposible"] != "no",
+        verified_on: new Date(),
+      });
+
+      verification.save((err, doc) => {
+        if (!err) console.log("success", "User added successfully!");
+        else console.log("Error during record insertion : " + err);
+      });
+
+      return(verification);
+    });;
   };
 
   
--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { VerificationController, verify_email_in_db } from "../controllers/verification_contoller.js";
 import { email_finder_request } from "../controllers/finder_controller.js";
+import { Verification } from "../models/verification.js";
 
 export const router = Router();
 const controller = new VerificationController();
-
 
 var request_counts = {
   klean_api: 0,
@@ -25,14 +25,22 @@ setInterval(() => {
 }, 10000);
 
 router.get("/", async (req, res) => {
-  let email = req.query.email;
+  Promise.all(req.body.emails.map((email) => verify_email(email))).then(
+    (results) => {
+      res.send(results);
+    }
+  );
+  
+});
+
+const verify_email = async (email) => {
 
   const cached_queries = await verify_email_in_db(email);
   console.log(cached_queries);
   if (cached_queries.length > 0) {
     console.log("Found");
-    res.send(cached_queries[0]);
-    return;
+    // res.send(cached_queries[0]);
+    return Promise.resolve(cached_queries[0]);
   }
 
   while (
@@ -45,13 +53,14 @@ router.get("/", async (req, res) => {
   if (request_counts["klean_api"] < api_limits["klean_api"]) {
     request_counts["klean_api"]++;
     console.log("KLEAN", request_counts);
-    controller.klean_api_request(res, email);
-  } else {
+
+    return controller.klean_api_request(email);
+  } 
+  else {
     request_counts["clearout_api"]++;
-    console.log("CLEAR", request_counts);
-    controller.clearout_email_verification(res, email);
+    return controller.clearout_email_verification(email);
   }
-});
+};
 
 router.get("/email-finder", (req, res) => {
   email_finder_request(res,req.body.name, req.body.domain);
